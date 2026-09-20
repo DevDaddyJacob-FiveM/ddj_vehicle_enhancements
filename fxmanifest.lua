cb26c2591089830a8ec6db19357d401abe5279c7
DevDaddyJacob <jacobwpaulin@gmail.com>
2026-09-20 19:04:10 -0400

    chore: set version for a alpha release

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ use_experimental_fxv2_oal "yes"
 
 author "DevDaddyJacob"
 description "A FiveM script which provides various enhancements to vehicles"
-version "1.0.0"
+version "0.1.0"
 
 dependencies {
 	"ddj_lib", -- version >= 1.4.0
